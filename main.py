5ae163cbdcc0650130262e154172b573b344caf0
NikitosMalinovskiy <nikitam23@nycstudents.net>
2024-05-06 08:44:04 -0400

    Day 1 commit
    Finding out how to integrate mouse into game: Using The Mouse In Pygame - Beginner Tutorial
    Setting up starter code

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("best project")
+import pygame
+import random
+import time
+
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 15)
+pygame.display.set_caption("Best Project")
+
+SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 1000
+size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode(size)
+
+
+run = True
+
+while run:
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+
+    screen.fill((50, 40, 98))
+    pygame.display.update()
+
+
+pygame.quit()
